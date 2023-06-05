7a4cafbbd161142bc97521ca52492f54147a65ca
Pruthvisinh <pruthvirajput97@gmail.com>
2023-06-05 17:14:22 +0530

    created a contect form

--- components/contect/contect-form.js
+++ components/contect/contect-form.js
@@ -1,23 +1,58 @@
 import classes from "./contect-form.module.css";
+import { useState, useEffect } from "react";
 
 const ContectForm = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+
+  const sendEmailHandler = (event) => {
+    event.preventDefault();
+    fetch("/api/contact", {
+      method: "POST",
+      body: JSON.stringify({ email, name, message }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    setEmail("");
+    setName("");
+    setMessage("");
+  };
+
   return (
     <section className={classes.contact}>
       <h1>How Can I Help You</h1>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={sendEmailHandler}>
         <div className={classes.controls}>
           <div className={classes.control}>
             <label htmlFor="email">Your Email</label>
-            <input type="email" id="email" required />
+            <input
+              type="email"
+              id="email"
+              required
+              onChange={(event) => setEmail(event.target.value)}
+            />
           </div>
           <div className={classes.control}>
             <label htmlFor="name">Your Name</label>
-            <input type="text" id="name" required />
+            <input
+              type="text"
+              id="name"
+              required
+              onChange={(event) => setName(event.target.value)}
+            />
           </div>
         </div>
         <div className={classes.control}>
           <label htmlFor="message">Your Message</label>
-          <textarea id="message" rows="5"></textarea>
+          <textarea
+            id="message"
+            rows="5"
+            required
+            onChange={(event) => setMessage(event.target.value)}
+          ></textarea>
         </div>
         <div className={classes.actions}>
           <button>Send Message</button>
