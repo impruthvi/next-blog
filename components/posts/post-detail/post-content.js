0a9e3966a3a8b6b33626f05c15650534d3344338
Pruthvisinh <pruthvirajput97@gmail.com>
2023-06-05 16:55:38 +0530

    read the code foramt from readme file

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -2,6 +2,8 @@ import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
 import ReactMarkdown from "react-markdown";
 import Image from "next/image";
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 const PostContent = ({ post }) => {
 
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
@@ -37,6 +39,18 @@ const PostContent = ({ post }) => {
       }
 
       return <p>{paragraph.children}</p>;
+    },
+
+    code(code) {
+      const { className, children } = code;
+      const language = className.split('-')[1]; // className is something like language-js => We need the "js" part here
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={children}
+        />
+      );
     },
 
   }
