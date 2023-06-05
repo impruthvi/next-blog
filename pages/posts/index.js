67f036cf2cd55adbf29f55526470f7d9048fdfff
Pruthvisinh <pruthvirajput97@gmail.com>
2023-06-05 11:21:55 +0530

    implemented a all post page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,10 +1,34 @@
+import AllPosts from "../../components/posts/all-post";
+
 const AllPostsPage = (props) => {
-  reurn(
-    <div>
-      <h1>All Posts</h1>
-      {/* <PostList posts={props.posts} /> */}
-    </div>
-  );
+  const DUMMY_POSTS = [
+    {
+      slug: "getting-started-with-nextjs",
+      title: "Getting Started with NextJS",
+      image: "getting-started-nextjs.png",
+      excerpt:
+        "NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
+      date: "2022-02-10",
+    },
+    {
+      slug: "getting-started-with-nextjs2",
+      title: "Getting Started with NextJS",
+      image: "getting-started-nextjs.png",
+      excerpt:
+        "NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
+      date: "2022-02-10",
+    },
+    {
+      slug: "getting-started-with-nextjs3",
+      title: "Getting Started with NextJS",
+      image: "getting-started-nextjs.png",
+      excerpt:
+        "NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
+      date: "2022-02-10",
+    },
+  ];
+
+  return (<AllPosts posts={DUMMY_POSTS} />);
 };
 
 export default AllPostsPage;
